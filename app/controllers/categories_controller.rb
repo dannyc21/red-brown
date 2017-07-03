5544015b1516b40d0fd432999e5ad85e24a623cf
Daniel <dannyclytus@gmail.com>
2017-07-03 01:11:36 -0400

    add category ti articles and articles to categories, style category section

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+before_action :require_admin, except: [:index, :show]
 
 	def index
 		@categories = Category.paginate(page: params[:page], per_page: 5)
@@ -18,8 +19,23 @@ class CategoriesController < ApplicationController
 		end
 	end
 
-	def show
+	def edit
+		@category = Category.find(params[:id])
+	end
 
+	def update
+		@category = Category.find(params[:id])
+		if @category.update(category_params)
+			flash[:success] = "Category name was successfully updated"
+			redirect_to category_path(@category)
+		else
+			render :edit
+		end
+	end
+
+	def show
+		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	private
@@ -27,4 +43,11 @@ class CategoriesController < ApplicationController
 			params.require(:category).permit(:name)
 		end
 
+		def require_admin
+			if !logged_in? || (logged_in? and !current_user.admin?)
+			flash[:danger] = "Only admins can perform that action"
+			redirect_to categories_path
+			end
+		end
+		
 end
